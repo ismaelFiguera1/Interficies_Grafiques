73ca7b40ee207216256bd58a0ad57c64af341ea3
Ismael Figuera <ismaelfiguera112@gmail.com>
2025-10-06 19:03:21 +0200

    p1

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        PictureBox1 = New PictureBox()
+        Label4 = New Label()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(78, 36)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(78, 149)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 1
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.BorderStyle = BorderStyle.FixedSingle
+        Label1.Font = New Font("Times New Roman", 24F, FontStyle.Bold)
+        Label1.Location = New Point(346, 44)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(47, 43)
+        Label1.TabIndex = 2
+        Label1.Text = "0"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.BorderStyle = BorderStyle.FixedSingle
+        Label2.Font = New Font("Times New Roman", 24F, FontStyle.Bold)
+        Label2.Location = New Point(489, 44)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(44, 43)
+        Label2.TabIndex = 3
+        Label2.Text = "0"
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label3
+        ' 
+        Label3.BorderStyle = BorderStyle.FixedSingle
+        Label3.Font = New Font("Times New Roman", 24F, FontStyle.Bold)
+        Label3.Location = New Point(630, 44)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(38, 43)
+        Label3.TabIndex = 4
+        Label3.Text = "0"
+        Label3.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(375, 121)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(362, 281)
+        PictureBox1.TabIndex = 5
+        PictureBox1.TabStop = False
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Arial", 18F, FontStyle.Bold)
+        Label4.ForeColor = Color.Purple
+        Label4.Location = New Point(12, 287)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(225, 29)
+        Label4.TabIndex = 6
+        Label4.Text = "Escurabutxaques7"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label4)
+        Controls.Add(PictureBox1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label4 As Label
+
+End Class
